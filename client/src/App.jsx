6649b5963d960dbb2f2553b635e57792bfc515f1
ramu siripalli <ramsiripalli2772@gmail.com>
2024-09-25 05:08:52 +0530

    Added Routing to Real Estate
    Added Signup,signin,profile,about,home to the routes page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/Signin';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
-const App = () => {
+
+export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
