472293eaab5e9dcead5fef35bfafedefbc2d4d8e
ramu siripalli <ramsiripalli2772@gmail.com>
2024-09-26 14:46:17 +0530

    adding of controllers and Routers of user
    adding of controllers and Routers of user

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const test = (req,res) => {
+    res.json({
+        message:"Hello Ramu Siripalli how are you , everyhting seems fine right"
+    });
+};
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose, { mongo } from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=> {
@@ -10,6 +11,7 @@ mongoose.connect(process.env.MONGO).then(()=> {
 });
 
 const app = express();
+app.use('/api/user', userRouter);
 
 app.listen(3000, ()=> {
     console.log('Server is Running on port 3000!!!')
