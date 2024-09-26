9fc07a8f61208ba53d6c0d5712dcfd2eb8a2c006
ramu siripalli <ramsiripalli2772@gmail.com>
2024-09-27 05:18:24 +0530

    Auth Controller saving to mongo Db with hashed password
    Auth Controller saving to mongo Db, we are catching Errors and we have hashed password

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose, { mongo } from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=> {
@@ -11,7 +12,9 @@ mongoose.connect(process.env.MONGO).then(()=> {
 });
 
 const app = express();
+app.use(express.json());
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 app.listen(3000, ()=> {
     console.log('Server is Running on port 3000!!!')
