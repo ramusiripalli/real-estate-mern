94a901d626c46310044624169d619e002d606f33
ramu siripalli <ramsiripalli2772@gmail.com>
2024-09-30 09:54:10 +0530

    Sign In Route
    Sign In Route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import User from '../models/user.model.js';
-
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 import bcryptjs from 'bcryptjs';
 
 
 export const signup = async(req,res,next) => {
 const { username,email,password } = req.body;
-const hashedPassword = bcryptjs.hashSync(password,10);
+const hashedPassword = bcryptjs.hashSync(password || '',10);
 const newUser = new User({ username,email,password:hashedPassword})
 try{
   await newUser.save()
@@ -15,5 +16,23 @@ catch (error){
 next(error);
 }
 
-  
+
 }
+
+export const signin = async(req,res,next) => {
+  const { email, password} = req.body;
+  try {
+const validUser = await User.findOne({ email});
+if(!validUser) return  next(errorHandler(404,'User Not Found'));
+const validPassword = bcryptjs.compareSync(password,validUser.password);
+
+if(!validPassword) return next(errorHandler(401,'Wrong Credentials!'));
+
+const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET);
+console.log(token);
+res.cookie('access_token',token, {httpOnly: true}).status(200).json(validUser);
+  } catch(error){
+    next(error)
+  }
+  }
+  
